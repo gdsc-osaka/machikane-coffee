ba186018d04d7fc0e0acb22ca83e1ea7584009cd
Harineko0 <harineko0927@gmail.com>
2023-08-07 01:21:57 +0900

    [Add] Added display_name, ShopStatus to shop/types.ts

--- src/modules/redux/shop/types.ts
+++ src/modules/redux/shop/types.ts
@@ -1,10 +1,13 @@
 import firebase from "firebase/compat";
 import Timestamp = firebase.firestore.Timestamp;
 
+export type ShopStatus = "active" | "pause_ordering"
+
 export type Shop = {
     id: string;
-    status: "active" | "pause_ordering";
+    status: ShopStatus;
     last_active_time: Timestamp;
+    display_name: string;
 };
 
 export function assertShop(data: any): asserts data is Shop {
@@ -13,6 +16,7 @@ export function assertShop(data: any): asserts data is Shop {
         !(
             typeof d?.id === "string" &&
             typeof d?.status === "string" &&
+            typeof d?.display_name === "string" &&
             d?.last_active_time instanceof Timestamp
         )
     ) {
