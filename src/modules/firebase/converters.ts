785b85a33f315fddd9799156309375919d429609
Harineko0 <harineko0927@gmail.com>
2023-08-06 21:52:17 +0900

    [Add] Added shopConverter

--- src/modules/firebase/converters.ts
+++ src/modules/firebase/converters.ts
@@ -1,15 +1,32 @@
 import firebase from "firebase/compat";
 import FirestoreDataConverter = firebase.firestore.FirestoreDataConverter;
 import {assertProduct, Product} from "../redux/product/types";
+import {assertShop, Shop} from "../redux/shop/types";
 
 export const productConverter: FirestoreDataConverter<Product> = {
     fromFirestore(snapshot: firebase.firestore.QueryDocumentSnapshot, options: firebase.firestore.SnapshotOptions): Product {
-        const data = snapshot.data(options);
+        // データに id を付加
+        const data = {...snapshot.data(options), id: snapshot.id};
         assertProduct(data);
         return data;
 
     },
     toFirestore(model: Product | Partial<Product>, options?: firebase.firestore.SetOptions): firebase.firestore.DocumentData {
-        return model;
+        // データから id を除去
+        return model as Omit<Product, "id">;
+    }
+}
+
+export const shopConverter: FirestoreDataConverter<Shop> = {
+    fromFirestore(snapshot: firebase.firestore.QueryDocumentSnapshot, options: firebase.firestore.SnapshotOptions): Shop {
+        // データに id を付加
+        const data = {...snapshot.data(options), id: snapshot.id};
+        assertShop(data);
+        return data;
+
+    },
+    toFirestore(model: Shop | Partial<Shop>, options?: firebase.firestore.SetOptions): firebase.firestore.DocumentData {
+        // データから id を除去
+        return model as Omit<Shop, "id">;
     }
 }
--- src/modules/redux/shop/types.ts
+++ src/modules/redux/shop/types.ts
@@ -6,3 +6,16 @@ export type Shop = {
     status: "active" | "pause_ordering";
     last_active_time: Timestamp;
 };
+
+export function assertShop(data: any): asserts data is Shop {
+    const d = data as Partial<Shop>; // 補完のためキャスト
+    if (
+        !(
+            typeof d?.id === "string" &&
+            typeof d?.status === "string" &&
+            d?.last_active_time instanceof Timestamp
+        )
+    ) {
+        throw new Error("data is not Shop type");
+    }
+}
