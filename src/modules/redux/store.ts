4c2238e7da71f2123263e904d9ceeadc4e1a071a
Harineko0 <harineko0927@gmail.com>
2023-08-07 01:02:54 +0900

    [Add] Added RootState to store.ts

--- src/modules/redux/store.ts
+++ src/modules/redux/store.ts
@@ -2,9 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import shopReducer from "./shop/shopSlice";
 import productReducer from "./product/productsSlice";
 
-export default configureStore({
+const store = configureStore({
     reducer: {
         shop: shopReducer,
         product: productReducer,
     }
 })
+
+export default store;
+export type RootState = ReturnType<typeof store.getState>
