51e48cdf672b4babd2e0064c236fa63d00b1e541
Harineko0 <harineko0927@gmail.com>
2023-08-09 15:22:42 +0900

    [Add] Added orderReducer to store.ts

--- src/modules/redux/store.ts
+++ src/modules/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import shopReducer from "./shop/shopSlice";
 import productReducer from "./product/productsSlice";
+import orderReducer from "./order/ordersSlice";
 
 const store = configureStore({
     reducer: {
         shop: shopReducer,
         product: productReducer,
+        order: orderReducer
     }
 })
 
