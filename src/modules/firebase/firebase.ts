915136a806c7dccbb711993be1d0aaf954029a7f
Harineko0 <harineko0927@gmail.com>
2023-08-12 23:21:10 +0900

    [Modify] Product に thumbnail_path, shorter_name, thumbnail_url を追加. [Fix] Firestore のドキュメントに不必要なプロパティ(id など)が含まれていたバグを修正.

--- src/modules/firebase/firebase.ts
+++ src/modules/firebase/firebase.ts
@@ -1,8 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import firebaseConfig from "./config";
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const storage = getStorage();
 export default app;
