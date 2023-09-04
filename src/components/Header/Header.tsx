93303bb80e7cb524b8c1e5f411a10876c675c428
yanoaa <yanoatsusan@gmail.com>
2023-09-04 10:57:58 +0900

    feat:header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,77 @@
 import React from "react";
+import { Image } from "@mui/icons-material";
+// import { useRouter } from 'next/router';
 
 const Header = () => {
-    return <div>This is header.</div>;
+
+  let titleText = "";
+  const routerPath = "user";
+
+  switch(routerPath) {
+    case "user":
+      titleText = 'コーヒー愛好会';
+      break;
+
+    // case 'admin':
+    //   titleText = '管理ページ';
+    //   break;
+
+    // case 'Barista':
+    //   titleText = 'ドリップ';
+    //   break;
+
+    // case 'Cashier':
+    //   titleText = 'レジ';
+    //   break;
+
+    // case 'Login':
+    //   titleText = 'コーヒー愛好会';
+    //   break;
+
+    default:
+      titleText = 'コーヒー愛好会';
+      break;
+
+  }
+
+  const imageStyle = {
+    width: '40px',
+    height: '26.4px',
+  };
+  const fontStyle = {
+    // textAlign: 'center',
+    fontFamily: 'Roboto',
+    fontSize: '18px',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    lineHeight: 'normal',
+    letterSpacing: '0.1px',
+  };
+  const headerStyle = {
+    display: 'flex',
+    // width: 412px;
+    // height: 60px;
+    padding: '0px 20px',
+    // flex-direction: column;
+    justifyContent: 'center',
+    alignIitems: 'flex-start',
+    gap: '10px',
+  }
+  const logoStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '10px',
+  }
+
+    return (
+      <div style={headerStyle}>
+        <div style={logoStyle}>
+          <img style={imageStyle} src="/images/logo192.png" />
+          <div style={fontStyle}>{titleText}</div>
+        </div>
+      </div>
+    );
   };
   
   export default Header;
