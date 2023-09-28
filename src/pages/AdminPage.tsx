55fc9cb6579ca8c29a99ee7ee146c5013c785c6f
Harineko0 <harineko0927@gmail.com>
2023-09-29 02:07:43 +0900

    [Add] AdminPageに注文一覧, 受取済み注文一覧を追加.

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -4,23 +4,36 @@ import {useSelector} from "react-redux";
 import {fetchProducts, selectAllProduct, selectProductStatus} from "../modules/redux/product/productsSlice";
 import {useAppDispatch} from "../modules/redux/store";
 import {useParams} from "react-router-dom";
-import {ProductAmount} from "../modules/redux/order/types";
+import {Order, ProductAmount} from "../modules/redux/order/types";
 import OrderForm from "../components/order/OrderForm";
-import {CircularProgress} from "@mui/material";
 import {
-    addOrder, fetchOrders,
+    Button,
+    CircularProgress,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle
+} from "@mui/material";
+import {
+    addOrder, deleteOrder, fetchOrders,
     selectOrderStatus,
     selectReceivedOrder,
-    selectCompletedOrder
+    selectUnreceivedOrder, updateOrder
 } from "../modules/redux/order/ordersSlice";
 import OrderList from "../components/order/OrderList";
 import ShopManager from "../components/order/ShopManager";
+import ReceivedOrderList from "../components/order/ReceivedOrderList";
 
 const AdminPage = () => {
+    const [openDelete, setOpenDelete] = useState(false);
+    const [orderToDelete, setOrderToDelete] = useState<Order | null>(null);
+
     const dispatch = useAppDispatch();
     const products = useSelector(selectAllProduct);
     const productStatus = useSelector(selectProductStatus);
-    const unreceivedOrders = useSelector(selectCompletedOrder);
+    const unreceivedOrders = useSelector(selectUnreceivedOrder);
+    const receivedOrders = useSelector(selectReceivedOrder);
     const orderStatus = useSelector(selectOrderStatus);
     const params = useParams();
     const shopId = params.shopId ?? '';
@@ -44,7 +57,7 @@ const AdminPage = () => {
 
     const onOrderAddClicked = async () => {
         const trueProductAmount = Object.assign({}, productAmount);
-        // ゼロの要素は排除する
+        // 量がゼロの要素は排除する
         for (const id in trueProductAmount) {
             if (trueProductAmount[id] == 0) {
                 delete trueProductAmount[id];
@@ -54,6 +67,31 @@ const AdminPage = () => {
         await dispatch(addOrder({shopId: shopId, rawOrder: {is_student: false, product_amount: trueProductAmount, status: "idle"}}));
     }
 
+    const handleReceiveOrder = (order: Order) => {
+        console.log("update")
+        dispatch(updateOrder({shopId, newOrder: {...order, status: "received"}}));
+    }
+
+    const handleUnreceiveOrder = (order: Order) => {
+        dispatch(updateOrder({shopId, newOrder: {...order, status: "completed"}}));
+    }
+
+    const handleDeleteOrder = (order: Order) => {
+        setOrderToDelete(order);
+        setOpenDelete(true);
+    }
+
+    const handleCloseDelete = () => {
+        setOpenDelete(false);
+    }
+
+    const handleDelete = () => {
+        if (orderToDelete != null) {
+            dispatch(deleteOrder({shopId, order: orderToDelete}));
+            setOpenDelete(false);
+        }
+    }
+
     return(
         productStatus == "succeeded" ?
             <RowLayout>
@@ -61,7 +99,27 @@ const AdminPage = () => {
                     <OrderForm products={products} onChangeAmount={onChangeAmount} productAmount={productAmount} onOrderAddClicked={onOrderAddClicked}/>
                     <ShopManager/>
                 </Column>
-                <OrderList orders={unreceivedOrders} onOrderUpdated={(id, order) => {}} products={products}/>
+                <OrderList orders={unreceivedOrders} products={products} onClickReceive={handleReceiveOrder} onClickDelete={handleDeleteOrder}/>
+                <ReceivedOrderList receivedOrders={receivedOrders} products={products} onClickUnreceive={handleUnreceiveOrder}/>
+                <Dialog open={openDelete}
+                        onClose={handleCloseDelete}
+                        aria-labelledby="order-delete-alert-dialog"
+                        aria-describedby="check-order-delete-alert">
+                    <DialogTitle id={"order-delete-alert-title"}>
+                        {orderToDelete?.index}番の注文を消去しますか？
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="delete-dialog-description">
+                            注文の消去は取り消せません
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleCloseDelete}>キャンセル</Button>
+                        <Button onClick={handleDelete} autoFocus>
+                            消去する
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </RowLayout>
             : <CircularProgress />
     )
