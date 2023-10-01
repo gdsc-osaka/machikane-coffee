c6161c8e5ce1702cec7297fa87860ebbac19881a
Harineko0 <harineko0927@gmail.com>
2023-10-01 20:27:21 +0900

    [Fix] プルリクの修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,41 @@
 import store from "./modules/redux/store";
 import Header from "./components/Header/Header";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AdminPage from "./pages/AdminPage";
 import User from "./pages/User";
 import Timer from "./pages/Timer";
-import { TestPage } from "./pages/Test";
+import {TestPage} from "./pages/Test";
 import Footer from "./components/Footer/Footer";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 import React from "react";
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-import { themeOptions } from "./themeOptions";
+import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import {themeOptions} from "./themeOptions";
 import AdminBaristaPage from "./pages/AdminBaristaPage";
 import LogInPage from "./pages/LogInPage";
 
 const App = () => {
-  const theme = createTheme(themeOptions);
+    const theme = createTheme(themeOptions);
 
-  return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
+    return (
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
 
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/:shopId/admin" Component={AdminPage} />
-            <Route path="/:shopId/admin-barista" Component={AdminBaristaPage} />
-            <Route path="/:shopId/user" Component={User} />
-            <Route path="/:shopId/timer" Component={Timer} />
-            <Route path="/:shopId/test" Component={TestPage} />
-            <Route path="/:shopId/login" Component={LogInPage} />
-          </Routes>
-        </BrowserRouter>
-        <Footer />
-      </ThemeProvider>
-    </Provider>
-  );
+                <BrowserRouter>
+                    <Header/>
+                    <Routes>
+                        <Route path="/:shopId/admin" Component={AdminPage}/>
+                        <Route path="/:shopId/admin-barista" Component={AdminBaristaPage}/>
+                        <Route path="/:shopId/user" Component={User}/>
+                        <Route path="/:shopId/timer" Component={Timer}/>
+                        <Route path="/:shopId/test" Component={TestPage}/>
+                        <Route path="/:shopId/login" Component={LogInPage}/>
+                    </Routes>
+                </BrowserRouter>
+                <Footer/>
+            </ThemeProvider>
+        </Provider>
+    );
 };
 
 export default App;
--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,128 +1,129 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, {useEffect, useLayoutEffect, useState} from "react";
 import styled from "styled-components";
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 import {
-  fetchProducts,
-  selectAllProduct,
-  selectProductStatus,
+    fetchProducts,
+    selectAllProduct,
+    selectProductStatus,
 } from "../modules/redux/product/productsSlice";
-import { useAppDispatch } from "../modules/redux/store";
-import { Navigate, redirect, useParams } from "react-router-dom";
-import { ProductAmount } from "../modules/redux/order/types";
+import {useAppDispatch} from "../modules/redux/store";
+import {Navigate, redirect, useParams} from "react-router-dom";
+import {ProductAmount} from "../modules/redux/order/types";
 import OrderForm from "../components/order/OrderForm";
-import { CircularProgress } from "@mui/material";
+import {CircularProgress} from "@mui/material";
 import {
-  addOrder,
-  fetchOrders,
-  selectOrderStatus,
-  selectReceivedOrder,
-  selectUnreceivedOrder,
+    addOrder,
+    fetchOrders,
+    selectOrderStatus,
+    selectReceivedOrder,
+    selectUnreceivedOrder,
 } from "../modules/redux/order/ordersSlice";
 import OrderList from "../components/order/OrderList";
 import ShopManager from "../components/order/ShopManager";
 import firebase from "src/modules/firebase/firebase";
 import "firebase/auth";
 
-import { getAuth, getIdTokenResult, onAuthStateChanged } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-import { isatty } from "tty";
+import {getAuth, getIdTokenResult, onAuthStateChanged} from "firebase/auth";
+import {useNavigate} from "react-router-dom";
+import {isatty} from "tty";
 
 const AdminPage = () => {
-  const dispatch = useAppDispatch();
-  const products = useSelector(selectAllProduct);
-  const productStatus = useSelector(selectProductStatus);
-  const unreceivedOrders = useSelector(selectUnreceivedOrder);
-  const orderStatus = useSelector(selectOrderStatus);
-  const params = useParams();
-  const shopId = params.shopId ?? "";
-  const [IsAdmin, setIsAdmin] = useState<boolean | undefined>(undefined);
+    const dispatch = useAppDispatch();
+    const products = useSelector(selectAllProduct);
+    const productStatus = useSelector(selectProductStatus);
+    const unreceivedOrders = useSelector(selectUnreceivedOrder);
+    const orderStatus = useSelector(selectOrderStatus);
+    const params = useParams();
+    const shopId = params.shopId ?? "";
+    const [IsAdmin, setIsAdmin] = useState<boolean | undefined>(undefined);
 
-  const [productAmount, setProductAmount] = useState<ProductAmount>({});
-  const onChangeAmount = (productId: string, amount: number) => {
-    setProductAmount({ ...productAmount, [productId]: amount });
-  };
+    const [productAmount, setProductAmount] = useState<ProductAmount>({});
+    const onChangeAmount = (productId: string, amount: number) => {
+        setProductAmount({...productAmount, [productId]: amount});
+    };
 
-  useEffect(() => {
-    const auth = getAuth();
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        user.getIdTokenResult(true).then((result) => {
-          if (result.claims.admin === true) {
-            // admin
-            console.log("admin");
-            setIsAdmin(true);
-          } else {
-            // user
-            console.log("user");
-            setIsAdmin(false);
-          }
+    useEffect(() => {
+        const auth = getAuth();
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                user.getIdTokenResult(true).then((result) => {
+                    if (result.claims.admin === true) {
+                        // admin
+                        console.log("admin");
+                        setIsAdmin(true);
+                    } else {
+                        // user
+                        console.log("user");
+                        setIsAdmin(false);
+                    }
+                });
+            }
         });
-      }
-    });
-    console.log(IsAdmin);
-  }, [IsAdmin]);
+        console.log(IsAdmin);
+    }, [IsAdmin]);
 
-  useEffect(() => {
-    if (productStatus == "idle" || productStatus == "failed") {
-      dispatch(fetchProducts(shopId));
-    }
-  }, [dispatch, productStatus]);
+    useEffect(() => {
+        if (productStatus == "idle" || productStatus == "failed") {
+            dispatch(fetchProducts(shopId));
+        }
+    }, [dispatch, productStatus]);
 
-  useEffect(() => {
-    if (orderStatus == "idle" || orderStatus == "failed") {
-      dispatch(fetchOrders(shopId));
-    }
-  }, [dispatch, orderStatus]);
+    useEffect(() => {
+        if (orderStatus == "idle" || orderStatus == "failed") {
+            dispatch(fetchOrders(shopId));
+        }
+    }, [dispatch, orderStatus]);
 
-  const onOrderAddClicked = async () => {
-    const trueProductAmount = Object.assign({}, productAmount);
-    // ゼロの要素は排除する
-    for (const id in trueProductAmount) {
-      if (trueProductAmount[id] == 0) {
-        delete trueProductAmount[id];
-      }
-    }
+    const onOrderAddClicked = async () => {
+        const trueProductAmount = Object.assign({}, productAmount);
+        // ゼロの要素は排除する
+        for (const id in trueProductAmount) {
+            if (trueProductAmount[id] == 0) {
+                delete trueProductAmount[id];
+            }
+        }
 
-    await dispatch(
-      addOrder({
-        shopId: shopId,
-        rawOrder: {
-          is_student: false,
-          product_amount: trueProductAmount,
-          status: "idle",
-        },
-      })
-    );
-  };
+        await dispatch(
+            addOrder({
+                shopId: shopId,
+                rawOrder: {
+                    is_student: false,
+                    product_amount: trueProductAmount,
+                    status: "idle",
+                },
+            })
+        );
+    };
 
-  return IsAdmin !== undefined ? (
-    IsAdmin ? (
-      productStatus === "succeeded" ? (
-        <RowLayout>
-          <Column>
-            <OrderForm
-              products={products}
-              onChangeAmount={onChangeAmount}
-              productAmount={productAmount}
-              onOrderAddClicked={onOrderAddClicked}
-            />
-            <ShopManager />
-          </Column>
-          <OrderList
-            orders={unreceivedOrders}
-            onOrderUpdated={(id, order) => {}}
-            products={products}
-          />
-        </RowLayout>
-      ) : (
-        <CircularProgress />
-      )
+    return IsAdmin !== undefined ? (
+        IsAdmin ? (
+            productStatus === "succeeded" ? (
+                <RowLayout>
+                    <Column>
+                        <OrderForm
+                            products={products}
+                            onChangeAmount={onChangeAmount}
+                            productAmount={productAmount}
+                            onOrderAddClicked={onOrderAddClicked}
+                        />
+                        <ShopManager/>
+                    </Column>
+                    <OrderList
+                        orders={unreceivedOrders}
+                        onOrderUpdated={(id, order) => {
+                        }}
+                        products={products}
+                    />
+                </RowLayout>
+            ) : (
+                <CircularProgress/>
+            )
+        ) : (
+            <Navigate to={"/" + shopId + "/user"}/>
+        )
     ) : (
-      <Navigate to={"/" + shopId + "/user"} />
-    )
-  ) : (
-    <CircularProgress />
-  );
+        <CircularProgress/>
+    );
 };
 
 const RowLayout = styled.div`
