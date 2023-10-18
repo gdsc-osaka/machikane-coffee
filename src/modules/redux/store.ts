069b08e961fdec8da5caa0549b9abd13eb14fc95
harineko0 <harineko0927@gmail.com>
2023-10-18 12:12:16 +0900

    [Add] useAppSelectorを追加, AppDispatchをexport

--- src/modules/redux/store.ts
+++ src/modules/redux/store.ts
@@ -2,7 +2,7 @@ import {configureStore} from '@reduxjs/toolkit'
 import shopReducer from "./shop/shopsSlice";
 import productReducer from "./product/productsSlice";
 import orderReducer from "./order/ordersSlice";
-import {useDispatch} from "react-redux";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 
 const store = configureStore({
     reducer: {
@@ -20,5 +20,6 @@ const store = configureStore({
 
 export default store;
 export type RootState = ReturnType<typeof store.getState>
-type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch
 export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = () => useSelector
