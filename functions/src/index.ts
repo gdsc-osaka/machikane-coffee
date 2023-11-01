e9c92ff52c95671d7823ad0da1835bb29b438d3c
Harineko0 <harineko0927@gmail.com>
2023-11-02 04:17:41 +0900

    [Fix] firebase functions がデプロイできないバグを修正.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,55 @@
-import { initializeApp } from 'firebase-admin';
-import {setAdminClaim} from "./auth";
-import {onEveryday} from "./db";
+const {onSchedule} = require("firebase-functions/v2/scheduler");
+const {logger} = require("firebase-functions");
+const {getFirestore} = require("firebase-admin/firestore");
 
-const app = initializeApp();
+// The Firebase Admin SDK to delete inactive users.
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-export const auth = app.auth();
-export const db = app.firestore();
+const firestore = getFirestore();
 
-exports.setAdminClaim = setAdminClaim;
-exports.onEveryday = onEveryday;
+/**
+ * 毎日0時に実行されるリセット関数
+ * 1. shops/{shopId}/products/{productId} の stock を 0 に設定する
+ */
+// @ts-ignore
+exports.onEveryday = onSchedule("every day 00:00", async (e) => {
+    const productsRef = firestore.collectionGroup('products');
+    const shopIds: String[] = []; /* productsから得られるデータからshopIdを格納する */
+
+    try {
+        const query = await productsRef.get();
+        // @ts-ignore
+        const productRefs = query.docs.map(doc => doc.ref);
+
+        const batch = firestore.batch();
+
+        for (const productRef of productRefs) {
+            batch.update(productRef, {
+                stock: 0
+            });
+
+            const shopId = productRef.parent.parent?.id;
+            if (shopId) shopIds.push(shopId);
+        }
+
+        for (const shopId of shopIds) {
+            const orderInfoRef = firestore.doc(`shops/${shopId}/info/order_info`);
+
+            batch.update(orderInfoRef, {
+                last_order_index: 0,
+                reset_at: firestore.FieldValue.serverTimestamp(),
+            })
+        }
+
+        try {
+            await batch.commit();
+            logger.log("Product.stock reset complete.");
+        } catch (e) {
+            logger.error("Unable to commit batch.", e);
+        }
+
+    } catch (e) {
+        logger.error("Unable to get products document.", e);
+    }
+})
