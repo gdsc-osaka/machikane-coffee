544dbb86321298c6b6370360ac60780f2416d0b2
Harineko0 <harineko0927@gmail.com>
2023-11-04 03:30:16 +0900

    [Modify] NeumoContainerの角丸を小さめに

--- src/components/neumo.tsx
+++ src/components/neumo.tsx
@@ -5,14 +5,14 @@ import {ReactNode} from "react";
 type NeumoType = 'flat' | 'pressed';
 
 const NeumoMotionDiv = styled(motion.div)<{ type?: NeumoType }>`
-  border-radius: 50px;
+  border-radius: 30px;
   box-shadow: ${(props) => props.type === 'pressed' ? 'inset' : ''} 10px 10px 30px #d9d5d9,
     ${(props) => props.type === 'pressed' ? 'inset' : ''} -10px -10px 30px #ffffff;
   padding: 20px;
   background-color: #FFFBFF;
 `
 const NeumoDiv = styled.div<{ type?: NeumoType }>`
-  border-radius: 50px;
+  border-radius: 30px;
   box-shadow: ${(props) => props.type === 'pressed' ? 'inset' : ''} 10px 10px 30px #d9d5d9,
     ${(props) => props.type === 'pressed' ? 'inset' : ''} -10px -10px 30px #ffffff;
   padding: 20px;
@@ -26,14 +26,14 @@ export const NeumoContainer = (props: { children: ReactNode, type?: NeumoType })
         {children}
     </NeumoDiv>
 
-    return (
-        <NeumoMotionDiv layout style={{height: "auto"}}
-                        type={type}
-                        initial={{opacity: 0}}
-                        animate={{opacity: 1}}
-                        exit={{opacity: 0}}
-                        transition={{type: "spring", stiffness: 600, damping: 40}}>
-            {children}
-        </NeumoMotionDiv>
-    )
+    // return (
+    //     <NeumoMotionDiv layout style={{height: "auto"}}
+    //                     type={type}
+    //                     initial={{opacity: 0}}
+    //                     animate={{opacity: 1}}
+    //                     exit={{opacity: 0}}
+    //                     transition={{type: "spring", stiffness: 600, damping: 40}}>
+    //         {children}
+    //     </NeumoMotionDiv>
+    // )
 }
